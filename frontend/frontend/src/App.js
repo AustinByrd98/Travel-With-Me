0381f853f1831f626f150fde7e9885f3d305c726
Ryan Strandberg <strandbergryan@gmail.com>
2023-12-27 13:14:01 -0700

    Resubmit

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -2,21 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/header.js";
 import NavBar from "./components/navBar.js";
-import { Tripcard } from './components/card.js'
+// import { Tripcard } from './components/card.js'
 
-import { Container, Row, Col } from 'react-bootsrap'
-
+// import { Container, Row, Col } from 'react-bootsrap'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <NavBar/>
-      <Container>
-        <Row>
-          <Card/>
-        </Row>
-      </Container>
     </div>
   );
 }
--- frontend/frontend/src/components/card.js
+++ frontend/frontend/src/components/card.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Card, Badge, Button } from 'react-bootstrap';
 
-export function Tripcard({ data }) {
+function TripCard({ data }) {
     return (
-        <Card className="h-100 shadow-sm bg-white rounded">
-            <Card.Img variant="top" src={data.image} />
+        <Card className="h-100 shadow-sm bg-white rounded">  
             <Card.Body className="d-flex flex-column">
                 <div className="d-flex mb-2 justify-content-between">
                     <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
                 </div>
-            </Card.Body> 
+            </Card.Body>
+            <Card.Img variant="top" src={data.image} /> 
         </Card>
     )
 }
