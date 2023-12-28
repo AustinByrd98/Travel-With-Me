6320f27973e47de8fd3ba0f75b48fd43b12563bc
agathaa <aamoako1120@gmail.com>
2023-12-27 20:48:22 -0500

    updated footer

--- frontend/frontend/src/components/footer.js
+++ frontend/frontend/src/components/footer.js
@@ -1,12 +1,24 @@
-import React from "react"
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Footer = () => {
-    return (
-    <ul>
-        <li>Edit</li>
-        <li>Delete</li>
-    </ul>
-    )
-}
+  return (
+    <div class="d-flex justify-content-center">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className="mr-auto justify-content-center">
+              <Nav.Link href="#edit">Edit</Nav.Link>
+              <Nav.Link href="#delete">Delete</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
