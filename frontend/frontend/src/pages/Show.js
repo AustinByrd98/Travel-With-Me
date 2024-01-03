ed1f8fd715b7262a2744fdf4e08a0db33128161f
agathaa <aamoako1120@gmail.com>
2024-01-02 20:21:06 -0500

    showpage completed

--- frontend/frontend/src/pages/Show.js
+++ frontend/frontend/src/pages/Show.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import { useState } from 'react'
-import NewForm from '../components/newForm'
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-const TripForm = (props) =>{
-  return(
-<>
-<NewForm/>
 
-</>
+const Show = (props) => {
+  const trips = props.trips 
+  const params = useParams()
+  const navigate = useNavigate()
+  const id = params.id
+  const trip = trips.find((trip) => trip._id === id)
 
-  )
-}
 
-export default TripForm
+   
+  return (
+    <div className='trips'>
+        <h2>{trips.tripName}</h2>
+        <h2>{trips.date}</h2>
+        <h2>{trips.peopleNumber}</h2>
+        <h2>{trips.budget}</h2>
+        <h2>{trips.foodPlan}</h2>
+        <h2>{trips.foodPricing}</h2>
+        <h2>{trips.lodging}</h2>
+        <h2>{trips.lodgingPrice}</h2>
+        <h2>{trips.tripLocation}</h2>
+        <h2>{trips.visits}</h2>
+        <h2>{trips.travelMeans}</h2>
+        <h2>{trips.travelMeansPrice}</h2>
+        <button onClick={navigate('/')}>Back To Trips</button>
+    </div>
+  );
+};
+
+export default Show;
