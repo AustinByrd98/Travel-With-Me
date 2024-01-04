42a521eea98ce622eb35e3b9024c9f207eee539a
agathaa <aamoako1120@gmail.com>
2024-01-03 19:07:48 -0500

    navbar and header imported

--- frontend/frontend/src/pages/index.js
+++ frontend/frontend/src/pages/index.js
@@ -1,8 +1,13 @@
+import Header from "frontend/frontend/public/components/header.js";
+import NavBar from '../components/navBar'
+
 const Index = (props) =>{
    return(
     props.trips.map((trips) => (
         <div key={trips._id} className='trips'>
             <h3>{trips.name}</h3>
+            <Header />
+            <NavBar />
         </div>
     ))
    )
